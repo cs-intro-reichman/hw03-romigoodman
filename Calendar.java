95fae6195e59a1f26b59eef45c1c7708547df8e6
romigoodman <romi.goodman@post.runi.ac.il>
2024-01-11 23:27:06 +0200

    dd

--- Calendar.java
+++ Calendar.java
@@ -1,82 +1,60 @@
-/** 
- * Prints the calendar of a given year
- */
+// /** 
+//  * Prints the calendar of a given year
+//  */
 public class Calendar {	
     // Starting the calendar on 1/1/1900
 	static int dayOfMonth = 1;   
 	static int month = 1;
 	static int year = 1900;
-	static int dayOfWeek = 2;     // 1.1.1900 was a Monday
-	static int nDaysInMonth = 31; // Number of days in January
+	static int dayOfWeek = 2;     
+	static int nDaysInMonth = 31; 
+
 	
-	/** 
-	 * Prints the calendars of all the years in the 20th century. Also prints the  
-	 * number of Sundays that occured on the first day of the month during this period.
-	 */
-	public static void main(String args[]) {
+		public static void main(String args[]) {
 		int chosenYear = Integer.parseInt(args[0]);
-		int sundayCount=0;
-		while (year<chosenYear-1){
+		while (year<=chosenYear){
+			if(year == chosenYear){
+				if (dayOfWeek==1) {
+				System.out.println(dayOfMonth + "/" + month + "/" + year + " Sunday");
+			} else { System.out.println(dayOfMonth + "/" + month + "/" + year);
+
+			}
+			}
 			advance();
 	 		}
-	 		while (year==chosenYear){
-	 			System.out.print(dayOfMonth + "/" + month + "/" + year);
-	 			if (dayOfWeek==7){
-	 				System.out.print("sunday");
-	 			}
-	 			advance();
-	 			System.out.println();
-	 		}
+	 	}
+
+	  private static void advance() {
+		if (dayOfWeek<7)
+		{
+		dayOfWeek++;
 		}
-	
-	 private static void advance() {
-	int daysInMonth = nDaysInMonth(month, year);
-	dayOfMonth++;
-	if (dayOfMonth>daysInMonth){
-		dayOfMonth=1;
-		month++;
+		else
+		{
+		dayOfWeek =1;
+		}
 	  
+	 if (dayOfMonth < nDaysInMonth(month, year)) {
+	 	dayOfMonth++;
+	 } else {
+	 	month++;
+	 	dayOfMonth = 1;
+	 }
 	 if (month>12){
 	 	month=1;
 	 	year++;
 	 }
-	}
-	dayOfWeek=(dayOfWeek%7)+1;
 }
-		 
-	   private static int countSundays() {
-        int sundayCount = 0;
-
-        // Reset the date counters
-        dayOfMonth = 1;
-        month = 1;
-        year = 1900;
-        dayOfWeek = 2;
-
-        // Iterate through each year in the 20th century
-        while (year <= 1999) {
-            // Check if the current day is a Sunday and the first day of the month
-            if (dayOfWeek == 7 && dayOfMonth == 1) {
-                sundayCount++;
-            }
-
-            // Move to the next day
-            advance();
-        }
-
-        return sundayCount;
-    }
 
 	private static boolean isLeapYear(int year) {
-	  boolean isLeapYear;
-		isLeapYear = ((year % 400) == 0);
-		isLeapYear = isLeapYear || (((year % 4) == 0) && ((year % 100) != 0));
-		return isLeapYear;
+	boolean isLeapYear;
+	isLeapYear = ((year % 400) == 0);
+	isLeapYear = isLeapYear || (((year % 4) == 0) && ((year % 100) != 0));
+	return isLeapYear;
 	}
-	 
-	
-	private static int nDaysInMonth(int month, int year) {
-		int daysInMonth;
+    
+    private static int nDaysInMonth(int month, int year) {
+	int daysInMonth=0;
 	if (month == 4 || month == 6 || month == 9 || month == 11) 
 	{
     daysInMonth = 30;
@@ -92,10 +70,9 @@ public class Calendar {
 		daysInMonth= 28;
      	}
      }	
-	else
-	{ 
-	daysInMonth = 31;
-	}
+	else if (month == 1 ||  month == 3 || month == 5 || month == 7 || month == 8| month == 10 || month == 12) { 
+	daysInMonth = 31; }
+
 	return daysInMonth;
-	}
+}
 }
